4209c7c41996c6fc21d7bb81045388ee5703a85b
wabreisch <wabreisch@quinnipiac.edu>
2016-04-01 10:42:09 -0400

    adds pin table

--- client/main.js
+++ client/main.js
@@ -1,32 +1,25 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+// import { Session } from 'meteor/session';
 
 import './main.html';
 
 Template.onOff.helpers({
   performPinRead: function (pin) {
-    var method = "GET";
-    var headers = {};
     var url = "http://ser492arduino.local/arduino/digital/" + pin + "/";
 
-    var returnResult = HTTP.call(method, url, headers, function (error, result) {
+    HTTP.call("GET", url, function (error, result) {
       if (!error) {
         returnResult = result.content;
-
-        console.log(returnResult);
-
+        $("#" + pin).text(returnResult);
         return returnResult;
       }
     });
-
-    console.log(returnResult);
-    return returnResult;
   }
 });
 
 Template.onOff.events({
   "click #on": function () {
-      var start = new Date().getTime();
       var headers = {};
       // headers['Access-Control-Allow-Origin'] = '"';
       
@@ -34,10 +27,7 @@ Template.onOff.events({
       // HTTP.get("http://ser492arduino.local/arduino/digital/13/1", {headers: {AccessControlAllowOrigin: '*'}}, function (error, result) {
       // Meteor.call("makeRESTCall", "http://ser492arduino.local/arduino/digital/13/1", function (error, result) {
         if (error) {
-          // handle error
           console.log("Handling some error...");
-          var elapsed = new Date().getTime() - start;
-          alert(result.content + " it took " + elapsed + " ms.");
         } else {
           return result;
         }
@@ -45,14 +35,10 @@ Template.onOff.events({
       // HTTP.get("http://ser492arduino.local/arduino/digital/13/1");
     },
     "click #off": function () {
-      var start = new Date().getTime();
       HTTP.get("http://ser492arduino.local/arduino/digital/13/0", function (error, result) {
       // Meteor.call("makeRESTCall", "http://ser492arduino.local/arduino/digital/13/1", function (error, result) {
         if (error) {
-          // handle error
           console.log("Handling some error...");
-          var elapsed = new Date().getTime() - start;
-          alert(result.content + " it took " + elapsed + " ms.");
         } else {
           return result;
         }
