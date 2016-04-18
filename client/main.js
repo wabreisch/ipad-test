e8976d7d022e569a00dbe9ffefe76d2fca6655a3
wabreisch <wabreisch@quinnipiac.edu>
2016-04-18 08:49:42 -0400

    Update to latest version of iPad prototype

--- client/main.js
+++ client/main.js
@@ -4,18 +4,24 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.onOff.helpers({
-  performPinRead: function (pin) {
-    var url = "http://ser492arduino.local/arduino/digital/" + pin + "/";
+var ipAddress = "http://10.131.52.34";
 
-    HTTP.call("GET", url, function (error, result) {
-      if (!error) {
-        returnResult = result.content;
-        $("#" + pin).text(returnResult);
-        return returnResult;
-      }
-    });
-  }
+Template.onOff.helpers({
+  // performPinRead: function (pin) {
+  //   var url = "10.133.54.4/arduino/digital/" + pin + "/";
+
+  //   console.log("http://" + url);
+
+  //   HTTP.call("GET", url, function (error, result) {
+  //     if (!error) {
+  //       returnResult = result.content;
+  //       $("#" + pin).text(returnResult);
+  //       return returnResult;
+  //     } else {
+  //       console.log(error);
+  //     }
+  //   });
+  // }
 });
 
 Template.onOff.events({
@@ -23,25 +29,83 @@ Template.onOff.events({
       var headers = {};
       // headers['Access-Control-Allow-Origin'] = '"';
       
-      HTTP.get("http://ser492arduino.local/arduino/digital/13/1", headers, function (error, result) {
-      // HTTP.get("http://ser492arduino.local/arduino/digital/13/1", {headers: {AccessControlAllowOrigin: '*'}}, function (error, result) {
-      // Meteor.call("makeRESTCall", "http://ser492arduino.local/arduino/digital/13/1", function (error, result) {
+      HTTP.call("GET", ipAddress + "/arduino/digital/13/1", headers, function (error, result) {
         if (error) {
-          console.log("Handling some error...");
+          console.log(error);
         } else {
           return result;
         }
       });
-      // HTTP.get("http://ser492arduino.local/arduino/digital/13/1");
     },
     "click #off": function () {
-      HTTP.get("http://ser492arduino.local/arduino/digital/13/0", function (error, result) {
-      // Meteor.call("makeRESTCall", "http://ser492arduino.local/arduino/digital/13/1", function (error, result) {
+      var headers = {};
+
+      HTTP.call("GET", ipAddress + "/arduino/digital/13/0", headers, function (error, result) {
         if (error) {
-          console.log("Handling some error...");
+          console.log(error);
         } else {
           return result;
         }
       });
+    },
+
+    "click #servoon": function () {
+      var headers = {};
+
+      HTTP.call("GET", ipAddress + "/arduino/servo/9/1", headers, function (error, result) {
+        if (error) {
+          console.log(error);
+        } else {
+          return result;
+        }
+      });
+    },
+
+    "click #servooff": function () {
+      var headers = {};
+
+      HTTP.call("GET", ipAddress + "/arduino/servo/9/0", headers, function (error, result) {
+        if (error) {
+          console.log(error);
+        } else {
+          return result;
+        }
+      });
+    },
+
+    "click #pumpon": function () {
+      var headers = {};
+
+      HTTP.call("GET", ipAddress + "/arduino/digital/4/1", headers, function (error, result) {
+        if (error) {
+          console.log(error);
+        } else {
+          return result;
+        }
+      });
+    },
+
+    "click #pumpoff": function () {
+      var headers = {};
+
+      HTTP.call("GET", ipAddress + "/arduino/digital/4/0", headers, function (error, result) {
+        if (error) {
+          console.log(error);
+        } else {
+          return result;
+        }
+      });
+    },
+
+    "change #slider": function () {
+      var sliderValue = $("#slider").val();
+      HTTP.call("GET", ipAddress + "/arduino/servo/9/" + sliderValue, {}, function (error, result) {
+        if (error) {
+          console.log(error);
+        } else {
+          return result;
+        }
+      });
+      $("#sliderValue").text($("#slider").val());
     }
 });
